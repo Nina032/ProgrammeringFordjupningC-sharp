98ee591fdf21ddd794e586709781601c70382826
Nevena Kicanovic <111436933+Nina032@users.noreply.github.com>
2024-01-19 12:03:00 +0100

    24_01_19

---
+++ PersonCSVfil/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+namespace PersonCSVfil
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int  Age { get; set; }
+        public string City { get; set; }
+
+        public Person(string name, int age, string city)
+        {
+            Name = name;
+            Age = age;
+            City = city;
+        }
+
+        public string ToCSVString()
+        {
+            return $"{Name}, {Age}, {City}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person 4 input
+            Console.WriteLine("Ange information för person ");
+            Console.Write("Namn: ");
+            string name4 = Console.ReadLine();
+
+            Console.Write("Ålder: ");
+            int age4 = int.Parse(Console.ReadLine());
+
+            Console.Write("Stad: ");
+            string city4 = Console.ReadLine();
+
+            //lägg till  några instancer av Person-klassen
+            Person person1 = new Person("Alice", 25, "Stockholm");
+            Person person2 = new Person("Alice", 25, "Stockholm");
+            Person person3 = new Person("Alice", 25, "Stockholm");
+            Person person4 = new Person(name4, age4, city4);
+
+
+            //Lista med personer
+            List<Person> people = new List<Person> { person1, person2, person3, person4};
+
+            //Spara data till CSV-fil
+            string csvFilePath = "people.csv";
+
+            using (StreamWriter writer = new StreamWriter(csvFilePath, false, Encoding.UTF8)) 
+            {
+                //Skriv header
+                writer.WriteLine("Name,Age,City");
+
+                //Skriv data för varje person
+                foreach (var person in people)
+                {
+                    writer.WriteLine(person.ToCSVString());
+                }
+
+            }
+
+            Console.WriteLine($"Data har sparats till {csvFilePath}");
+
+
+            //Läser från CSV-fil och skriver till konsolen
+
+
+        }
+    }
+}
---
+++ Övningar_24_01_19/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Övningar_24_01_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Övning 1
+            //string fileName = @"mytekst.txt";
+
+            //try
+            //{
+            //    //Ta bort filen om den finns
+            //    if (File.Exists(fileName))
+            //    {
+            //        File.Delete(fileName);
+            //    }
+
+            //    Console.Write("Create a file med text and read the file:\n");
+            //    Console.WriteLine("-------------------------------------------\n");
+
+            //    //Skapar och skriver till filen
+            //    using (StreamWriter fileStr = File.CreateText(fileName)) 
+            //    {
+            //        fileStr.WriteLine(" Hello and Welcome");
+            //        fileStr.WriteLine(" It is the first content");
+            //        fileStr.WriteLine(" of the text file mytekst.txt");
+            //    }
+            //    //Läs från filen
+            //    using (StreamReader sr = File.OpenText(fileName))
+            //    {
+            //        string s = "";
+            //        Console.WriteLine(" Here is the content of the file mytekst.txt : ");
+            //        while ((s = sr.ReadLine()) != null)
+            //        {
+            //            Console.WriteLine(s);
+            //        }
+            //        Console.WriteLine("");
+            //    }
+            //}
+            //catch (Exception MyExcep) 
+            //{
+            //    Console.WriteLine(MyExcep.ToString());
+            //}
+
+            //Övning 2
+
+            string fileName = @"mytekst.txt";
+
+            Console.Write("Create a file med write an array of strings : \n");
+            Console.WriteLine("-------------------------------------------\n");
+
+            //Ta bort filen om den finns
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+
+            Console.WriteLine(" Input number of lines to write in the file  :");
+            int n = int.Parse(Console.ReadLine());
+            
+            string [] ArrLines = new string[n];
+            Console.WriteLine($" Input {n} strings below: \n");
+
+            for (int i = 0; i<n; i++)
+            {
+                Console.WriteLine($"Input line {i+1}");
+                ArrLines[i] = Console.ReadLine();
+            }
+
+            File.WriteAllLines(fileName, ArrLines);
+
+            Console.WriteLine();
+
+            using (StreamReader sr = File.OpenText(fileName))
+            {
+                string s = "";
+                Console.WriteLine(" Here is the content of the file mytekst.txt : ");
+                while ((s = sr.ReadLine()) != null)
+                {
+                Console.WriteLine(s);
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
